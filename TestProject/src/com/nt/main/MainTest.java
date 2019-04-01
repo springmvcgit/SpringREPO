a7ca746027f793822160b87fb91cd3bf0780e0a3
DEV1 <DEV1@gmail.com>
2019-04-01 09:45:23 +0530

    CHANGE ON MSG

--- TestProject/src/com/nt/main/MainTest.java
+++ TestProject/src/com/nt/main/MainTest.java
@@ -2,7 +2,7 @@ package com.nt.main;
 
 public class MainTest {
 	public static void main(String[] args) {
-		System.out.println("my name is khan IT IS DEVELOPER 1");
+		System.out.println("my name is khan IT IS DEVELOPER 1 I HAVE CHANGER");
 	}
 
 }
