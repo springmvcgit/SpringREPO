a90bf5308f6d630ff18b4b3e961c60731253ea9b
DEV1 <DEV1@gmail.com>
2019-04-01 09:51:24 +0530

    TL

--- TestProject/src/com/nt/main/MainTest.java
+++ TestProject/src/com/nt/main/MainTest.java
@@ -2,7 +2,7 @@ package com.nt.main;
 
 public class MainTest {
 	public static void main(String[] args) {
-		System.out.println("my name is khan IT IS DEVELOPER 1 I NNN HAVE CHANGER");
+		System.out.println("FF my name is khan IT IS DEVELOPER 1 I NNN HAVE CHANGER");
 	}
 
 }
