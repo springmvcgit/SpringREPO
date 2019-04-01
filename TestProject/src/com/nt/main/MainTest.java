f5d15e5847f00432393bbf7e465dd83536a990a0
DEV1 <DEV1@gmail.com>
2019-04-01 09:52:15 +0530

    F

--- TestProject/src/com/nt/main/MainTest.java
+++ TestProject/src/com/nt/main/MainTest.java
@@ -2,7 +2,7 @@ package com.nt.main;
 
 public class MainTest {
 	public static void main(String[] args) {
-		System.out.println("my name is khan IT IS FDF DEVELOPER 1 IT MAY  BE CONGLICT");
+		System.out.println("WW my name is khan IT IS FDF DEVELOPER 1 IT MAY  BE CONGLICT");
 	}
 
 }
