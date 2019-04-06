cb56fbe61cebbba12faaefc60325b045a374039d
DEV1 <DEV1@gmail.com>
2019-04-06 09:04:13 +0530

    dev1

--- TestProject/src/com/nt/main/MainTest.java
+++ TestProject/src/com/nt/main/MainTest.java
@@ -4,7 +4,7 @@ public class MainTest {
 	public static void main(String[] args) {
 
 		System.out.println("WW my name is khan IT IS FDF DEVELOPER 1 IT MAY  BE CONGLICT");
-		System.out.println("dev1");
+		System.out.println("dev1 is commiting code-----");
 	}
 
 }
