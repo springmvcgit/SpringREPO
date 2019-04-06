9ff51a45bb4c11b582d9e459956507e5257dc62f
DEV1 <DEV1@gmail.com>
2019-04-06 08:58:30 +0530

    TL_Commit

--- TestProject/src/com/nt/main/MainTest.java
+++ TestProject/src/com/nt/main/MainTest.java
@@ -4,9 +4,6 @@ public class MainTest {
 	public static void main(String[] args) {
 
 		System.out.println("WW my name is khan IT IS FDF DEVELOPER 1 IT MAY  BE CONGLICT");
-		System.out.println(" i am adding something");
-		System.out.println("kuch v");
-		System.out.println("kkkkkkkkkkk");
-	}
+		}
 
 }
